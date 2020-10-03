145bccad2a4d0aa6efc9a6553ab5493aa257fd6f
Toshi0619 <chiaki.afro.7.pf@gmail.com>
2020-10-03 16:30:56 +0900

    Issue4

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,8 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
+    # ストロングパラーメーターでrequireメソッドに入れるのはモデル。:calendersから:planに修正
   end
 
   def getWeek
